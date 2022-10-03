e7f5e17593da099303c7c3d57a4b137d4825ea3d
RaviYadav3003 <114741865+RaviYadav3003@users.noreply.github.com>
2022-10-03 16:45:04 +0530

    complete all feature

---
+++ Fun_with_Triangles/script.js
@@ -0,0 +1,28 @@
+// let angleOne = document.querySelector(".angle1").value;
+let angleTwo = document.querySelector("#Angle2");
+// let angleThree = document.querySelector("#Angle3");
+let isTriangleBtn = document.querySelector(".isTriangleButton");
+// var x = document.getElementById("myNumber").value;
+let lastLineBtn = document.querySelector(".lastLine");
+var angleInputs = document.querySelectorAll(".angle");
+console.log(angleInputs[0].value);
+const onisTriangleClicked = function () {
+  console.log("new=", angleInputs[0].value);
+  console.log("angle2=", angleTwo.value);
+  let angleIn0 = angleInputs[0].value;
+  let angleIn1 = angleInputs[1].value;
+  let angleIn2 = angleInputs[2].value;
+
+  const angleNum0 = Number.parseInt(angleIn0);
+  const angleNum1 = Number.parseInt(angleIn1);
+  const angleNum2 = Number.parseInt(angleIn2);
+  // console.log(angleLast0, angleLast1, angleLast2);
+  var AddedValue = angleNum0 + angleNum1 + angleNum2;
+  if (AddedValue == 180) {
+    return (lastLineBtn.innerText = "It's A Triangle");
+  } else {
+    return (lastLineBtn.innerText = "Not Forming a Triangle ");
+  }
+};
+
+isTriangleBtn.addEventListener("click", onisTriangleClicked);
